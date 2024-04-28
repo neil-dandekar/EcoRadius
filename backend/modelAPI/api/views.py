003d93d8876f45d8bd1a0c2323ab6a4ade8a8ca9
Neil Dandekar <neildandekar10@gmail.com>
2024-04-28 06:15:24 -0700

    .

--- backend/modelAPI/api/views.py
+++ backend/modelAPI/api/views.py
@@ -5,12 +5,18 @@ from model.classifier import Classifier
 
 @api_view(["POST"])
 def classify_input(request):
+    print("helloooo")
     if "image" not in request.data:
         return Response({"error": "No file uploaded"}, status=400)
     image_data = request.data["image"]
-
+    print(image_data)
+    print("hello")
     classifier = Classifier()
+    print("hello2")
+
     prediction = classifier.predict(image_data)
+
+    print("PREDICTION:", prediction)
 
     # Send back the prediction
     return Response({"prediction": prediction})

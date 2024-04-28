88e27c0925113f2187a6775e90146b3169038051
Neil Dandekar <neildandekar10@gmail.com>
2024-04-28 06:05:28 -0700

    .

--- frontend/src/Scan.tsx
+++ frontend/src/Scan.tsx
@@ -4,15 +4,36 @@ import React, { useState } from "react";
 const Scan = () => {
   const [image, setImage] = useState("");
 
+  const getPrediction = ()
+
   const capture = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files ? event.target.files[0] : null;
     if (file) {
       const reader = new FileReader();
       reader.onload = function (evt: ProgressEvent<FileReader>) {
-        setImage(evt.target!.result as string);
+        const base64image = evt.target!.result as string
+        setImage(base64image);
+        sendToBackend(base64image);
       };
       reader.readAsDataURL(file);
     }
+  };
+
+  const sendToBackend = (base64image: string) => {
+    fetch('http://127.0.0.1:8000/api/classify/', {  // Ensure this URL is correct and points to your Django server
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ image: base64image })
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
   };
 
   return (

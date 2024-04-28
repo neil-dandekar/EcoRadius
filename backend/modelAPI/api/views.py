8450def3533479e89da6b1beab4efdcf2ece4c1c
Neil Dandekar <neildandekar10@gmail.com>
2024-04-28 03:49:14 -0700

    OOP classifier model

--- backend/modelAPI/api/views.py
+++ backend/modelAPI/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import Response
+from rest_framework.decorators import api_view
+from ..model.classifier import Classifier
 
-# Create your views here.
+
+@api_view(["POST"])
+def classify_input(request):
+    if "file" not in request.FILES:
+        return Response({"error": "No file uploaded"}, status=400)
+
+    classifier = Classifier()
+    # image_path = "inputs/test3.jpg"
+    prediction = classifier.predict(image_path)
+
+    # Send back the prediction
+    return Response({"prediction": prediction})
